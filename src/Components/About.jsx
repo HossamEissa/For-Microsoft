806bc3c344f9456c7cbaa47e0aac53d7ddf8807a
Hossam Essa <hossameissa822001@gmail.com>
2023-09-20 22:28:41 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,28 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/about.webp";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "There are some developers who doing brainstorming";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a back-end developer with php and laravel , have good skills in problem solving using C++ and also have a bronze medalist in ECPC (Egyption Collegiate Programming Contest and qualifing to compete in ACPC ( Arab and Africa Collegiate Programming Contest) finally former leader at Google DSC Tanta University";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Problem Solving",
+  "leadership",
+  "C | C++ | PHP | HTML | CSS",
+  "Data Sturcture ",
+  "Algorithms",
+  "APIs",
+  "Git",
 ];
 
 /**
@@ -45,7 +46,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "Never give up , Great things take time , Be patient";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/protofolio.webp";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,28 +31,22 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "My Graduation Project",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "This mobile application to help people with nearset ambulance , and take him to the nearset hosptil JUST ONE CLICK",
+    url: "https://github.com/HossamEissa/Emerg-e-Care-Graduation-Project-",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Invoices Project",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "The Invoices project is used to create , edit , update , delete you invoices for any perpouse and another things",
+    url: "https://github.com/HossamEissa/Inovices-Project-laravel",
   },
   {
-    title: "My Resume Site",
+    title: "Blog System",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "This is a basic website using php pure to create , delete , update Post and add comments to this post.",
+    url: "https://github.com/HossamEissa/Blog-System-PHP-Pure",
   },
 ];
 
